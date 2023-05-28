49d71c4d9f7b7c89c75c928732377a3d02e8ecb9
hanane20sa <greendino06@gmail.com>
2023-05-28 13:05:38 -0700

    utils.c

--- utils.c
+++ utils.c
@@ -14,7 +14,8 @@ int is_printable(char c)
 	return (0);
 }
 /**
- * append_hexa_code - Concatenate hexadecimal representation of ascci code to buffer
+ * append_hexa_code - Concatenate hexadecimal
+ * representation of ascci code to buffer
  * @buffer: Character array of appending.
  * @j: Starting index for  appending.
  * @ascii_code: ASSCI CODE to be added
@@ -23,21 +24,22 @@ int is_printable(char c)
 int append_hexa_code(char ascii_code, char buffer[], int j)
 {
 	char map_to[] = "0123456789ABCDEF";
+
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
 	buffer[j++] = '\\';
 	buffer[j++] = 'x';
 
-	buffer[j++] = map_to [ ascii_code / 16];
-        buffer[j] = map_t o[ ascii_code % 16];
+	buffer[j++] = map_to[ascii_code / 16];
+	buffer[j] = map_to[ascii_code % 16];
 
 	return (3);
 }
 
 /**
  * is_digit - Check if the a chararacter is a digit
- *
+ * @c: arg
  * Return: 1 if c is a digit, 0 otherwise
  */
 int is_digit(char c)
@@ -49,7 +51,7 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Converts a number to the specified size
+ * convert_size_unsgnd - Converts a number to the specified size
  * @num: Number to be converted
  * @size: Size indicating  the target type of conversion.
  *
@@ -67,7 +69,8 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 
 /**
  * convert_size_unsgnd - Converts a number to the specified size
- *
+ * @size: size
+ * @num: number
  * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
