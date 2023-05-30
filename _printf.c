ab151aa0105cc74f2f12d3905343b67af11560fa
hanane20sa <greendino06@gmail.com>
2023-05-30 08:13:11 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -32,7 +32,6 @@ int _printf(const char *format, ...)
 	else
 	{
 	print_buffer(buffer, &buff_ind);
-	format++;
 	flags = get_flags(format, &j);
 	width = get_width(format, &j, list);
 	precision = get_precision(format, &j, list);
